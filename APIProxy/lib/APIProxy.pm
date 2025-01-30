e59e0a511d706180bc313e73461010978a6d4698
Dave Cross <dave@davecross.co.uk>
2025-01-30 12:40:19 +0000

    Back out changes

--- APIProxy/lib/APIProxy.pm
+++ APIProxy/lib/APIProxy.pm
@@ -7,22 +7,5 @@ get '/' => sub {
   template 'index' => { 'title' => 'APIProxy' };
 };
 
-get '/api/:api' => sub {
-  my $api = route_parameters->get('api');
-
-  warn "$api\n";
-
-  template 'index' => { 'title' => 'APIProxy' };
-};
-
-get '/api/:api/**' => sub {
-  my $api = route_parameters->get('api');
-
-  my $rest = splat;
-
-  warn "$api -> $rest\n";
-
-  template 'index' => { 'title' => 'APIProxy' };
-};
 
 true;
