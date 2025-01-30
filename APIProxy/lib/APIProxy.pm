ea88a4718939d894b5ac8b74c75c15036ef3cea1
Dave Cross <dave@davecross.co.uk>
2025-01-30 16:57:00 +0000

    Make it basically work

--- APIProxy/lib/APIProxy.pm
+++ APIProxy/lib/APIProxy.pm
@@ -8,27 +8,53 @@ get '/' => sub {
   template 'index' => { 'title' => 'APIProxy' };
 };
 
-get '/gmap_search' => sub {
-  my $query = query_parameters->get('q');
-  return status_bad_request("Missing 'q' parameter") unless $query;
+my %apis = (
+  gmap_search => {
+    params => [ 'q' ],
+    url    => 'https://www.google.com/maps/embed/v1/search?key=KEY_HERE',
+    key    => 'GOOGLE_MAPS_API_KEY',
+  },
+);
 
-  my $api_key = $ENV{'GOOGLE_MAPS_API_KEY'};
+get '/:api' => sub {
+  my $api_name = route_parameters->get('api');
+  my $api = $apis{$api_name};
+
+  return 404 unless $api;
+
+  my $params;
+  for (@{ $api->{params} }) {
+    $params->{$_} = query_parameters->get($_);
+    return status_bad_request("Missing 'q' parameter") unless $params->{$_};
+  }
+
+  my $api_key = $ENV{$api->{key}};
   return status_bad_request("Missing API key") unless $api_key;
 
-  my $url = "https://www.google.com/maps/embed/v1/search?key=$api_key&q=$query";
+  my $url = $api->{url};
+
+  $url =~ s/KEY_HERE/$api_key/;
+
+  if (keys %$params) {
+    $url .= '&';
+
+    $url .= join '&', map { "$_=$params->{$_}" } keys %$params;
+  }
+
   my $ua = LWP::UserAgent->new;
   my $response = $ua->get($url);
 
-  if ($response->is_success) {
-    content_type 'text/html';
-    return $response->decoded_content;
-  } else {
-    return status_bad_request("Failed to fetch data from Google Maps API");
-  }
+  #if ($response->is_success) {
+    content_type $response->header('Content-Type');
+    return $response->content;
+  #} else {
+  #  return status_bad_request("Failed to fetch data from Google Maps API");
+  #}
 };
 
 sub status_bad_request {
   my ($message) = @_;
+  warn "$message\n";
   status 'bad_request';
   return { error => $message };
 }
