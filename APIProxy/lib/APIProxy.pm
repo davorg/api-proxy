fe0340fb88d449368038db1cfcc611f685594241
Dave Cross <dave@davecross.co.uk>
2025-01-30 12:38:54 +0000

    Start making it work

--- APIProxy/lib/APIProxy.pm
+++ APIProxy/lib/APIProxy.pm
@@ -4,7 +4,25 @@ use Dancer2;
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index' => { 'title' => 'APIProxy' };
+  template 'index' => { 'title' => 'APIProxy' };
+};
+
+get '/api/:api' => sub {
+  my $api = route_parameters->get('api');
+
+  warn "$api\n";
+
+  template 'index' => { 'title' => 'APIProxy' };
+};
+
+get '/api/:api/**' => sub {
+  my $api = route_parameters->get('api');
+
+  my $rest = splat;
+
+  warn "$api -> $rest\n";
+
+  template 'index' => { 'title' => 'APIProxy' };
 };
 
 true;
